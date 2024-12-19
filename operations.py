709878af545546cdae48b8b5209a101816633d65
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-12-19 11:01:26 +0530

    tuples

---
+++ operations.py
@@ -0,0 +1,15 @@
+tuplex = ("tuple",False,1,2,3)
+print(tuplex)
+
+tuplex = (4,6,2,8,1,9)
+print(tuplex)
+
+tuple1 = (10,12,14,10,15)
+print(tuple1.count(10))
+
+tuplex = (2,4,1,3,5,8,6,9,7,9,6,8,1)
+_slice = tuplex[4:8]
+print(_slice)
+
+_slice = tuplex[:9]
+print(_slice)
